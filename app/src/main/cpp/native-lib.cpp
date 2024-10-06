08f632ce78105801ba4a49bb1083638745871c25
techy4shri <sushri4tech@gmail.com>
2024-10-07 00:17:39 +0530

    An android app for Insolvency Professionals

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_in_solve_MainActivity_stringFromJNI(
+Java_com_example_in_1solve_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Welcome to In-Solve";
     return env->NewStringUTF(hello.c_str());
 }
--- app/src/main/java/com/example/in_solve/MainActivity.kt
+++ app/src/main/java/com/example/in_solve/MainActivity.kt
@@ -7,28 +7,27 @@ import com.example.in_solve.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+     binding = ActivityMainBinding.inflate(layoutInflater)
+     setContentView(binding.root)
 
         // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
+        binding.entrance.text = stringFromJNI()
     }
-
     /**
-     * A native method that is implemented by the 'in_solve' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
+      * A native method that is implemented by the 'in_solve' native library,
+      * which is packaged with this application.
+      */
+     external fun stringFromJNI(): String
 
-    companion object {
-        // Used to load the 'in_solve' library on application startup.
-        init {
-            System.loadLibrary("in_solve")
-        }
-    }
+     companion object {
+         // Used to load the 'in_solve' library on application startup.
+         init {
+             System.loadLibrary("in_solve")
+         }
+     }
 }
